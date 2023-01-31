4042bbb84763b93f36bef0d58728f1a17d2c28aa
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-01-31 18:26:22 +0100

    all parts added | done

---
+++ src/components/PasswordField/PasswordField.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import {
+  ContentContainer,
+  MainContainer,
+  PasswordContainer,
+  PasswordInput,
+  PasswordStrengthSection,
+  SectionsList,
+} from './PasswordField.styled';
+
+const PasswordField = () => {
+  const [password, setPassword] = useState('');
+
+  const checkStrenth = password => {
+    let strength = 0;
+    if (password === '') {
+      return strength;
+    }
+    if (password.length < 8) {
+      strength = 5;
+      console.log(strength);
+      return strength;
+    }
+    if (password.match(/[A-Za-z]/)) {
+      strength += 1;
+    }
+    if (password.match(/([0-9])/)) {
+      strength += 1;
+    }
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~,+,-])/)) {
+      strength += 1;
+    }
+    return strength;
+  };
+
+  return (
+    <MainContainer>
+      <ContentContainer>
+        <h1>Test Your Password</h1>
+        <PasswordContainer>
+          <PasswordInput
+            type="text"
+            value={password}
+            onChange={evt => setPassword(evt.currentTarget.value)}
+          />
+          <SectionsList>
+            <PasswordStrengthSection
+              strength={checkStrenth(password)}
+            ></PasswordStrengthSection>
+            <PasswordStrengthSection
+              strength={checkStrenth(password)}
+            ></PasswordStrengthSection>
+            <PasswordStrengthSection
+              strength={checkStrenth(password)}
+            ></PasswordStrengthSection>
+          </SectionsList>
+        </PasswordContainer>
+      </ContentContainer>
+    </MainContainer>
+  );
+};
+
+export default PasswordField;
---
+++ src/components/PasswordField/PasswordField.styled.js
@@ -0,0 +1,78 @@
+const { default: styled } = require('@emotion/styled');
+
+const strengthPresets = {
+  0: 'none',
+  5: 'tooShort',
+  1: 'easy',
+  2: 'medium',
+  3: 'hard',
+};
+
+const nthOptions = {
+  tooShort: '-n+3',
+  easy: '-n+1',
+  medium: '-n+2',
+  hard: '-n+3',
+};
+
+export const MainContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  fontsize: 40px;
+  color: #010101;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PasswordInput = styled.input`
+  width: 100%;
+  font-size: 28px;
+  border: 1px solid rgba(33, 33, 33, 0.2);
+  border-radius: 4px;
+`;
+
+export const PasswordContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  width: 90%;
+`;
+
+export const SectionsList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const PasswordStrengthSection = styled.div`
+  flex-basis: 33%;
+  height: 8px;
+  margin: 10px 3px;
+  background-color: gray;
+  transition: background-color 450ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:nth-of-type(${({ strength }) => nthOptions[strengthPresets[strength]]}) {
+    background-color: ${({ strength }) => {
+      switch (strengthPresets[strength]) {
+        case 'tooShort':
+          return 'red';
+        case 'easy':
+          return 'red';
+        case 'medium':
+          return 'yellow';
+        case 'hard':
+          return 'green';
+        default:
+          break;
+      }
+    }};
+`;
